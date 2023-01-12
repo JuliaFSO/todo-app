1c5494154cc7eab118bc8eeb64236304a5e8275c
JuliaFSO <juliafso@gmail.com>
2023-01-12 17:27:06 -0500

    Adds the home component

---
+++ frontend/src/main/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = props => (
+  <div className='container text-center' >
+    <h1><i className='fa fa-calendar-check-o'></i> ToDo-App</h1>
+    <h2>This application allows users to create, manage, and track their daily tasks and responsibilities!</h2>
+
+  </div>
+);
+
+export default Home;
