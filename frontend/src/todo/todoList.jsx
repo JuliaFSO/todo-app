d25f04f477e2711beb9a77187bc1f0c4809f4fee
JuliaFSO <juliafso@gmail.com>
2023-01-13 15:56:46 -0500

    Adds todoList and function handleRemove

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const todoList = props => (
-  <div>
-    <h1>Todo List</h1>
-  </div>
-);
+import IconButton from '../template/iconButton';
+
+const todoList = props => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td>{todo.description}</td>
+        <td><IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)} /></td>
+      </tr>
+    ));
+  };
+
+  return (
+      <div className=''>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderRows()}
+          </tbody>
+        </table>
+      </div>
+  );
+};
 
 export default todoList;
