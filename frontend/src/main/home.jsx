cdf60ae08f330b353940fb2fc7c9fd9bf5e3c0bd
JuliaFSO <juliafso@gmail.com>
2023-01-14 21:48:46 -0500

    Adds image to home

--- frontend/src/main/home.jsx
+++ frontend/src/main/home.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = props => (
   <div className='container text-center' >
+    <img src="https://images.pexels.com/photos/7319184/pexels-photo-7319184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={350} />
     <h1><i className='fa fa-calendar-check-o'></i> ToDo-App</h1>
-    <h2>This application allows users to create, manage, and track their daily tasks and responsibilities!</h2>
+    <h2>Organize your life, and never forget about something you need to do again!</h2>
+    <h3>Create, manage, and track your daily tasks and responsibilities with <strong>ToDo-App</strong>.</h3>
 
   </div>
 );
