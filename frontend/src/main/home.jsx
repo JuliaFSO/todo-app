6bf0247b7853e43f7fd50f45d8820485cb315bfb
JuliaFSO <juliafso@gmail.com>
2023-01-16 10:41:28 -0500

    Adds handleSearch, about text

--- frontend/src/main/home.jsx
+++ frontend/src/main/home.jsx
@@ -4,8 +4,8 @@ const Home = props => (
   <div className='container text-center' >
     <img src="https://images.pexels.com/photos/7319184/pexels-photo-7319184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={350} />
     <h1><i className='fa fa-calendar-check-o'></i> ToDo-App</h1>
-    <h2>Organize your life, and never forget about something you need to do again!</h2>
-    <h3>Create, manage, and track your daily tasks and responsibilities with <strong>ToDo-App</strong>.</h3>
+    <h1>Get it done with our app!!</h1>
+    <h3>Streamline your daily to-dos and reach your responsibilities with ease using our app.</h3>
 
   </div>
 );
