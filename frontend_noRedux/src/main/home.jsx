1b2952b82fa940cd0cc2448e07e230226b38f7b5
JuliaFSO <juliafso@gmail.com>
2023-01-26 17:20:18 -0500

    ToDoApp Frontend with no Redux

---
+++ frontend_noRedux/src/main/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = props => (
+  <div>
+    <div className='pageHeader'>
+        <i className='fa fa-calendar-check-o'></i> ToDo-App
+    </div>
+    <div className='content'>
+      <h1>Get it all done with this app!</h1>
+        <h3>Streamline your daily to-dos and reach your responsibilities with ease.</h3>
+    </div>
+  </div>
+);
+
+export default Home;
