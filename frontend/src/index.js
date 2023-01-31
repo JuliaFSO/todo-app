5c052d98984d0bbcfbf1025218e853eb71e23c02
JuliaFSO <juliafso@gmail.com>
2023-01-31 15:45:28 -0500

    Adds applyMiddleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import storeConfig from './main/reducers';
+import { applyMiddleware } from 'redux';
+import promise from 'redux-promise';
 
 import App from './main/app';
 
+const store = applyMiddleware(promise)(storeConfig);
+
 ReactDOM.render(
-  ( <Provider store={storeConfig()}>
+  ( <Provider store={store()}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
